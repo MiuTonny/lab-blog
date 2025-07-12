b28e72c93ec3301d681758b19e08a0d765e5a89c
Milton Tavares <tonny2@tony.>
2025-07-12 18:15:18 -0400

    Finalized blog project with test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+// src/App.test.jsx
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Header from './components/Header';
+import React from 'react';
 
-test('renders blog name from prop', () => {
-  render(<Header name="Milton's Test Blog" />);
-  const headerElement = screen.getByText(/Milton's Test Blog/i);
-  expect(headerElement).toBeInTheDocument();
+describe('Header', () => {
+  it("renders blog name from prop", () => {
+    render(<Header name="Milton's Test Blog" />);
+    const headerElement = screen.getByText(/Milton's Test Blog/i);
+    expect(headerElement).toBeInTheDocument();
+  });
 });
